df9b18845e75f32728cfa5a02e2ca54cd60dd5e9
Alex Larson <>
2022-11-19 00:15:17 -0600

    Implemented Hann Filter and FFT algorithm.

--- src/main/kotlin/colorListener/sound/fft/FftAlgorithm.kt
+++ src/main/kotlin/colorListener/sound/fft/FftAlgorithm.kt
@@ -1,12 +1,30 @@
 package colorListener.sound.fft
 
+import org.jtransforms.fft.DoubleFFT_1D
+import java.lang.Math.sqrt
+
 interface FftAlgorithmInterface {
     fun process(signal: DoubleArray): DoubleArray
 }
 
 class FftAlgorithm : FftAlgorithmInterface {
 
+    // Reference: the "Extract Real" section from https://github.com/wendykierp/JTransforms/issues/4#issue-142313147
     override fun process(signal: DoubleArray): DoubleArray {
-        TODO("Not yet implemented")
+        val doubleFFT = DoubleFFT_1D(signal.size.toLong())
+        doubleFFT.realForward(signal)
+        return createAmplitudes(signal)
+    }
+
+    private fun createAmplitudes(signal: DoubleArray): DoubleArray {
+        val amplitudes = DoubleArray(signal.size / 2)
+
+        for (i in amplitudes.indices) {
+            val re = signal[i * 2]
+            val im = signal[i * 2 + 1]
+            amplitudes[i] = sqrt(re * re + im * im) / signal.size
+        }
+
+        return amplitudes
     }
 }
