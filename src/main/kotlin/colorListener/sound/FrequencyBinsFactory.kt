1b5367c21262f0f35a9add5b4686011f683370e3
Alex Larson <>
2022-11-19 00:18:14 -0600

    no message

--- src/main/kotlin/colorListener/sound/FrequencyBinsFactory.kt
+++ src/main/kotlin/colorListener/sound/FrequencyBinsFactory.kt
@@ -4,10 +4,10 @@ interface FrequencyBinsFactoryInterface {
     fun createFrom(sampleRate: Int, sampleSize: Int, amplitudes: DoubleArray): List<FrequencyBin>
 }
 
-class FrequencyBinsFactory: FrequencyBinsFactoryInterface {
+class FrequencyBinsFactory : FrequencyBinsFactoryInterface {
 
     override fun createFrom(sampleRate: Int, sampleSize: Int, amplitudes: DoubleArray): List<FrequencyBin> {
-        var frequencyBins = mutableListOf<FrequencyBin>()
+        val frequencyBins = mutableListOf<FrequencyBin>()
 
         for (i in amplitudes.indices) {
             val frequency = i * sampleRate.toDouble() / sampleSize.toDouble()
