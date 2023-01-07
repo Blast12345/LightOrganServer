1e498c988c4fe0c65a20068992f55fa573d6481e
Alex Larson <>
2023-01-07 13:43:21 -0600

    Implemented magnitude normalizer

--- src/main/kotlin/sound/signalProcessing/MagnitudeNormalizer.kt
+++ src/main/kotlin/sound/signalProcessing/MagnitudeNormalizer.kt
@@ -7,15 +7,13 @@ interface MagnitudeNormalizerInterface {
 class MagnitudeNormalizer : MagnitudeNormalizerInterface {
 
     override fun normalize(magnitudes: DoubleArray, sampleSize: Int): DoubleArray {
-        TODO("Not yet implemented")
+        return magnitudes.map { magnitude ->
+            normalize(magnitude, sampleSize)
+        }.toDoubleArray()
     }
 
-    // TODO: Normalize later
-    // TODO: Frequency Bin Sanitizer
-//    private fun calculateNormalizedMagnitude(real: Double, imaginary: Double, sampleSize: Int): Float {
-//        val magnitude = calculateMagnitude(real, imaginary)
-//        val normalizedMagnitude = magnitude / sampleSize
-//        return normalizedMagnitude.toFloat()
-//    }
+    private fun normalize(magnitude: Double, sampleSize: Int): Double {
+        return magnitude / sampleSize
+    }
 
 }
--- src/test/kotlin/sound/signalProcessing/MagnitudeNormalizerTests.kt
+++ src/test/kotlin/sound/signalProcessing/MagnitudeNormalizerTests.kt
@@ -1,4 +1,23 @@
 package sound.signalProcessing
 
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Test
+
 class MagnitudeNormalizerTests {
+
+    private val magnitudes = doubleArrayOf(2.0, 4.0)
+    private val sampleSize = 2
+
+    private fun createSUT(): MagnitudeNormalizer {
+        return MagnitudeNormalizer()
+    }
+
+    @Test
+    fun `normalize the magnitudes by dividing the magnitude by the sample size`() {
+        val sut = createSUT()
+        val normalizedMagnitudes = sut.normalize(magnitudes, sampleSize)
+        val expected = doubleArrayOf(1.0, 2.0)
+        assertArrayEquals(expected, normalizedMagnitudes, 0.001)
+    }
+
 }
