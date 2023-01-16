09639d2a3db22a81da16ce53d6b11adb2e7e8daa
Alex Larson <>
2023-01-15 21:33:52 -0600

    Reintroduced a weighted average calculation

--- src/test/kotlin/sound/frequencyBins/dominantFrequency/DominantFrequencyCalculatorTests.kt
+++ src/test/kotlin/sound/frequencyBins/dominantFrequency/DominantFrequencyCalculatorTests.kt
@@ -1,17 +1,16 @@
 package sound.frequencyBins.dominantFrequency
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
 import sound.frequencyBins.FrequencyBin
 
 class DominantFrequencyCalculatorTests {
 
-    private val bin1 = FrequencyBin(10F, 1F)
-    private val bin2 = FrequencyBin(20F, 2F)
+    private val bin1 = FrequencyBin(10F, 0F)
+    private val bin2 = FrequencyBin(20F, 4F)
     private val bin3 = FrequencyBin(30F, 1F)
-    private val bin4 = FrequencyBin(40F, 1F)
-    private val bin5 = FrequencyBin(50F, 1F)
-    private val frequencyBins = listOf(bin1, bin2, bin3, bin4, bin5)
+    private val bin4 = FrequencyBin(40F, 0F)
 
 
     private fun createSUT(): DominantFrequencyCalculator {
@@ -19,10 +18,19 @@ class DominantFrequencyCalculatorTests {
     }
 
     @Test
-    fun `the dominant frequency is the bin with the highest magnitude`() {
+    fun `return the weighted average of the frequencies`() {
         val sut = createSUT()
+        val frequencyBins = listOf(bin1, bin2, bin3, bin4)
         val actual = sut.calculate(frequencyBins)
-        assertEquals(20F, actual)
+        assertEquals(22F, actual)
+    }
+
+    @Test
+    fun `return null when the total magnitude is zero`() {
+        val sut = createSUT()
+        val frequencyBins = listOf(bin1, bin4)
+        val actual = sut.calculate(frequencyBins)
+        assertNull(actual)
     }
 
 }
