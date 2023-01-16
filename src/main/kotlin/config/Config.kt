b91d48ba9d3a6d9247f5ecb5cc607934dc2d4d6a
Alex Larson <>
2023-01-16 15:47:29 -0600

    Basic config documentation

--- src/main/kotlin/config/Config.kt
+++ src/main/kotlin/config/Config.kt
@@ -1,10 +1,21 @@
 package config
 
-// TODO: Document the config
 interface Config {
+    // Helps identify a hue for a given frequency
     val colorWheel: ColorWheel
+
+    // Defines the upper range of bass frequency bins
     val highPassFilter: HighPassFilter
+
+    // Then number of audio samples to use to identify frequencies
+    // Higher values increase accuracy, but also increase latency
     val sampleSize: Int
+
+    // We interpolate the sample size to a higher value to reveal obscured frequencies,
+    // increasing the accuracy of frequency calculations.
+    // Higher values and values that are not powers of 2 increase processing time.
     val interpolatedSampleSize: Int
+
+    // Helps determine how we estimate the dominant frequency's magnitude
     val magnitudeEstimationStrategy: MagnitudeEstimationStrategy
 }
