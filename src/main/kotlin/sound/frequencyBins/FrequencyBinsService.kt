3dffa190bc80926e36d3cf4f1a32e23f6d194c63
Alex Larson <>
2023-01-15 21:20:48 -0600

    Defining the sample size in the config

--- src/main/kotlin/sound/frequencyBins/FrequencyBinsService.kt
+++ src/main/kotlin/sound/frequencyBins/FrequencyBinsService.kt
@@ -4,8 +4,6 @@ import sound.fft.RelativeMagnitudesCalculator
 import sound.fft.RelativeMagnitudesCalculatorInterface
 import sound.frequencyBins.filters.FrequencyBinListDenoiser
 import sound.frequencyBins.filters.FrequencyBinListDenoiserInterface
-import sound.frequencyBins.filters.SupportedFrequencyBinsFilter
-import sound.frequencyBins.filters.SupportedFrequencyBinsFilterInterface
 import sound.input.samples.AudioSignal
 import sound.signalProcessing.SignalProcessor
 import sound.signalProcessing.SignalProcessorInterface
@@ -19,39 +17,31 @@ class FrequencyBinsService(
     private val relativeMagnitudesCalculator: RelativeMagnitudesCalculatorInterface = RelativeMagnitudesCalculator(),
     private val granularityCalculator: GranularityCalculatorInterface = GranularityCalculator(),
     private val frequencyBinListFactory: FrequencyBinListFactoryInterface = FrequencyBinListFactory(),
-    private val supportedFrequencyBinsFilter: SupportedFrequencyBinsFilterInterface = SupportedFrequencyBinsFilter(),
     private val frequencyBinListDenoiser: FrequencyBinListDenoiserInterface = FrequencyBinListDenoiser()
 ) : FrequencyBinsServiceInterface {
 
-    private val lowestSupportedFrequency = 20F
-
     override fun getFrequencyBins(audioSignal: AudioSignal): FrequencyBinList {
-        val processedSamples = getProcessedSamples(audioSignal)
-        val magnitudes = getMagnitudes(processedSamples)
+        val magnitudes = getMagnitudes(audioSignal)
         val granularity = getGranularity(magnitudes.size, audioSignal.sampleRate)
-        val allBins = getAllBins(magnitudes, granularity)
-        val supportedBins = getSupportedBins(allBins)
-        return getDenoisedBins(supportedBins)
+        val frequencyBins = getFrequencyBins(magnitudes, granularity)
+        return getDenoisedBins(frequencyBins)
+    }
+
+    private fun getMagnitudes(audioSignal: AudioSignal): DoubleArray {
+        val processedSamples = getProcessedSamples(audioSignal)
+        return relativeMagnitudesCalculator.calculate(processedSamples)
     }
 
     private fun getProcessedSamples(audioSignal: AudioSignal): DoubleArray {
-        return signalProcessor.process(audioSignal, lowestSupportedFrequency)
-    }
-
-    private fun getMagnitudes(signal: DoubleArray): DoubleArray {
-        return relativeMagnitudesCalculator.calculate(signal)
+        return signalProcessor.process(audioSignal)
     }
 
     private fun getGranularity(numberOfBins: Int, sampleRate: Float): Float {
         return granularityCalculator.calculate(numberOfBins, sampleRate)
     }
 
-    private fun getAllBins(magnitudes: DoubleArray, granularity: Float): FrequencyBinList {
+    private fun getFrequencyBins(magnitudes: DoubleArray, granularity: Float): FrequencyBinList {
         return frequencyBinListFactory.create(magnitudes, granularity)
-    }
-
-    private fun getSupportedBins(frequencyBins: FrequencyBinList): FrequencyBinList {
-        return supportedFrequencyBinsFilter.filter(frequencyBins, lowestSupportedFrequency)
     }
 
     private fun getDenoisedBins(frequencyBins: FrequencyBinList): FrequencyBinList {
