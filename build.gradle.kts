351690c5af85f3654c8c5a6cfec748d9e5589e0b
Alex Larson <>
2023-03-02 22:04:52 -0600

    Migrated to gradle KTS

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.github.wendykierp:JTransforms:3.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    dependencies {
+        testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
+        testImplementation("io.mockk:mockk:1.13.3")
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    }
+}
