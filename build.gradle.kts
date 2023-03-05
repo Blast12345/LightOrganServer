2b97c7574334479e95c3bba3f7efde2f338d5333
Alex Larson <>
2023-03-05 17:06:59 -0600

    no message

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,14 +9,14 @@ repositories {
 dependencies {
     implementation("com.github.wendykierp:JTransforms:3.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+
+    // Unit Testing
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
+    testImplementation("io.mockk:mockk:1.13.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 }
 
 tasks.test {
     useJUnitPlatform()
-    dependencies {
-        testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
-        testImplementation("io.mockk:mockk:1.13.3")
-        testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
-        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
-    }
 }
