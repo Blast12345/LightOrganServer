642734c2b33664c21ab48aacd341da08c43b36a9
Alex Larson <>
2023-03-30 21:52:15 -0500

    Reimplementing stat calculations

--- src/main/kotlin/gui/ViewModel.kt
+++ src/main/kotlin/gui/ViewModel.kt
@@ -3,6 +3,7 @@ package gui
 import LightOrgan
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
+import config.ConfigSingleton
 import sound.input.Input
 import sound.input.finder.InputFinder
 
@@ -20,6 +21,9 @@ class DefaultInputFactory {
 // TODO: Test me
 class ViewModel {
 
+    private var input = DefaultInputFactory().create()
+    private var lightOrgan = LightOrgan(input)
+
     val startAutomatically = mutableStateOf(false)
     val isRunning = mutableStateOf(false)
     val color = mutableStateOf(Color.Black)
@@ -27,8 +31,12 @@ class ViewModel {
     val lowestDiscernibleFrequency = mutableStateOf("")
     val frequencyResolution = mutableStateOf("")
     val serverLatency = mutableStateOf("")
-    private var input = DefaultInputFactory().create()
-    private var lightOrgan = LightOrgan(input)
+
+    init {
+        updateDurationOfAudioUsed()
+        updateLowestDiscernibleFrequency()
+        updateFrequencyResolution()
+    }
 
     fun startAutomaticallyPressed(value: Boolean) {
         // TODO:
@@ -43,6 +51,93 @@ class ViewModel {
     fun stopPressed() {
         lightOrgan.stop()
         isRunning.value = false
+    }
+
+    private fun updateDurationOfAudioUsed() {
+        durationOfAudioUsed.value = formattedDurationOfAudioUsed()
+    }
+
+    private fun formattedDurationOfAudioUsed(): String {
+        return formatted(
+            value = calculateSecondsOfAudioUsed(),
+            unitOfMeasure = " seconds"
+        )
+    }
+
+    private fun calculateSecondsOfAudioUsed(): Float {
+        return SecondsOfAudioUsedCalculator().calculate(
+            sampleRate = input.dataLine.format.sampleRate,
+            sampleSize = ConfigSingleton.sampleSize,
+            numberOfChannels = input.dataLine.format.channels
+        )
+    }
+
+    private fun updateLowestDiscernibleFrequency() {
+        lowestDiscernibleFrequency.value = formattedLowestDiscernibleFrequency()
+    }
+
+    private fun formattedLowestDiscernibleFrequency(): String {
+        return formatted(
+            value = calculateLowestDiscernibleFrequency(),
+            unitOfMeasure = " Hz"
+        )
+    }
+
+    private fun calculateLowestDiscernibleFrequency(): Float {
+        return LowestDiscernibleFrequencyCalculator().calculate(
+            secondsOfAudioUsed = calculateSecondsOfAudioUsed()
+        )
+    }
+
+    private fun updateFrequencyResolution() {
+        frequencyResolution.value = formattedFrequencyResolution()
+    }
+
+    private fun formattedFrequencyResolution(): String {
+        return formatted(
+            value = calculateFrequencyResolution(),
+            unitOfMeasure = " Hz"
+        )
+    }
+
+    private fun calculateFrequencyResolution(): Float {
+        return FrequencyResolutionCalculator().calculate(
+            sampleSize = ConfigSingleton.sampleSize,
+            sampleRate = input.dataLine.format.sampleRate,
+            numberOfChannels = input.dataLine.format.channels
+        )
+    }
+
+    // Helper
+    private fun formatted(value: Float, unitOfMeasure: String): String {
+        val formattedValue = String.format("%.2f", value)
+        return "$formattedValue $unitOfMeasure"
+    }
+
+}
+
+
+class SecondsOfAudioUsedCalculator {
+
+    fun calculate(sampleSize: Int, sampleRate: Float, numberOfChannels: Int): Float {
+        return sampleSize.toFloat() / sampleRate / numberOfChannels.toFloat()
+    }
+
+}
+
+class LowestDiscernibleFrequencyCalculator {
+
+    fun calculate(secondsOfAudioUsed: Float): Float {
+        return 1 / secondsOfAudioUsed
+    }
+
+}
+
+class FrequencyResolutionCalculator {
+
+    // TODO: Verify this calculation
+    fun calculate(sampleRate: Float, sampleSize: Int, numberOfChannels: Int): Float {
+        return sampleRate / sampleSize.toFloat() * numberOfChannels.toFloat()
     }
 
 }
