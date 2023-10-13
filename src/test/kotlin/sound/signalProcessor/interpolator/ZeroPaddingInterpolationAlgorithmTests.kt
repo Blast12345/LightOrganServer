8d84dcb5ef568e60ef6c16e550d29be548f5d0b0
Alex Larson <>
2023-10-12 20:01:46 -0500

    no message

--- src/test/kotlin/sound/signalProcessor/interpolator/ZeroPaddingInterpolationAlgorithmTests.kt
+++ src/test/kotlin/sound/signalProcessor/interpolator/ZeroPaddingInterpolationAlgorithmTests.kt
@@ -8,8 +8,6 @@ import org.junit.jupiter.api.assertThrows
 
 class ZeroPaddingInterpolationAlgorithmTests {
 
-    private val samples = doubleArrayOf(1.0, 2.0)
-
     @AfterEach
     fun teardown() {
         clearAllMocks()
@@ -22,6 +20,7 @@ class ZeroPaddingInterpolationAlgorithmTests {
     @Test
     fun `when a desired size is larger than the samples passed in then zero-pad to the desired size`() {
         val sut = createSUT()
+        val samples = doubleArrayOf(1.0, 2.0)
 
         val actual = sut.applyTo(samples, 4)
 
@@ -32,16 +31,18 @@ class ZeroPaddingInterpolationAlgorithmTests {
     @Test
     fun `when a desired size is equal to than the samples passed in then return the input`() {
         val sut = createSUT()
+        val samples = doubleArrayOf(3.0, 4.0)
 
         val actual = sut.applyTo(samples, 2)
 
-        val expected = doubleArrayOf(1.0, 2.0)
+        val expected = doubleArrayOf(3.0, 4.0)
         assertArrayEquals(expected, actual, 0.001)
     }
 
     @Test
     fun `when a desired size is smaller than the samples passed in then throw an exception`() {
         val sut = createSUT()
+        val samples = doubleArrayOf(5.0, 6.0)
 
         assertThrows<Exception>(
             executable = { sut.applyTo(samples, 1) },
