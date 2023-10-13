c7ee19ded2e72f52f528155a0a3a2f00ae1fea41
Alex Larson <>
2023-10-12 21:45:26 -0500

    Working band pass curve.

--- src/main/kotlin/sound/frequencyBins/filters/BandPassFilter.kt
+++ src/main/kotlin/sound/frequencyBins/filters/BandPassFilter.kt
@@ -1,11 +1,12 @@
 package sound.frequencyBins.filters
 
-import extensions.between
 import sound.frequencyBins.FrequencyBin
 import sound.frequencyBins.FrequencyBinList
-import kotlin.math.min
+import kotlin.math.ln
+import kotlin.math.max
 
-// TODO: Find a more natural filter
+// TODO: Find a more natural filter?
+// The linear rolloff may lead to unusual averaging results
 class BandPassFilter {
 
     fun filter(
@@ -21,25 +22,32 @@ class BandPassFilter {
     }
 
     private fun FrequencyBinList.removeBinsLessThan(frequency: Float): FrequencyBinList {
-        return this.filter { it.frequency < frequency }
+        return this.filter { it.frequency > frequency }
     }
 
     private fun FrequencyBinList.removeBinsGreaterThan(frequency: Float): FrequencyBinList {
-        return this.filter { it.frequency > frequency }
+        return this.filter { it.frequency < frequency }
     }
 
     private fun FrequencyBinList.applyFilter(filter: Crossover): FrequencyBinList {
         val filteredBins = mutableListOf<FrequencyBin>()
 
         for (bin in this) {
-            val range = filter.cornerFrequency - filter.stopFrequency
-            val rawMultiplier = (bin.frequency - filter.stopFrequency) / range
-            val multiplier = rawMultiplier.between(0F, 1F)
-            val filteredMagnitude = bin.magnitude * min(multiplier, 1F)
+            val logPosition = log(bin.frequency) - log(filter.cornerFrequency)
+            val logWidth = log(filter.stopFrequency) - log(filter.cornerFrequency)
+            val multiplier = max(logPosition / logWidth, 0F)
+            val magnitudeDecrease = bin.magnitude * multiplier
+            val filteredMagnitude = bin.magnitude - magnitudeDecrease
             filteredBins.add(FrequencyBin(bin.frequency, filteredMagnitude))
         }
 
         return filteredBins
+    }
+
+    private fun log(float: Float): Float {
+        val double = float.toDouble()
+        val log = ln(double)
+        return log.toFloat()
     }
 
 }
--- src/test/kotlin/sound/frequencyBins/filters/BandPassFilterTests.kt
+++ src/test/kotlin/sound/frequencyBins/filters/BandPassFilterTests.kt
@@ -1,11 +1,16 @@
 package sound.frequencyBins.filters
 
+import org.junit.jupiter.api.Test
+import sound.frequencyBins.FrequencyBin
+import sound.frequencyBins.FrequencyBinList
+
 // TODO: Test
 class BandPassFilterTests {
 
-//    private val highPassFrequency = 20F
-//    private val lowPassFrequency = 120F
-//    private val frequencyBinList = listOf(
+    private val lowCrossover = Crossover(13.75F, 27.5F)
+    private val highCrossover = Crossover(110F, 55F)
+
+    //    private val frequencyBinList = listOf(
 //        FrequencyBin(10F, 1F),
 //        FrequencyBin(20F, 1F),
 //        FrequencyBin(100F, 1F),
@@ -13,13 +18,11 @@ class BandPassFilterTests {
 //        FrequencyBin(1000F, 1F)
 //    )
 //
-//    private fun createSUT(): BandPassFilter {
-//        return BandPassFilter(
-////            highPassCornerFrequency = highPassFrequency,
-////            lowPassCornerFrequency = lowPassFrequency
-//        )
-//    }
-//
+    private fun createSUT(): BandPassFilter {
+        return BandPassFilter()
+    }
+
+    //
 //    @Test
 //    fun `omit frequencies outside of band`() {
 //        val sut = createSUT()
@@ -31,29 +34,29 @@ class BandPassFilterTests {
 //        assertEquals(expected, actualFrequencies)
 //    }
 //
-//    @Test
-//    fun `magnitudes approach 0 as frequencies approach low pass frequency`() {
-//        val sut = createSUT()
-//
-//
-//        val actual = sut.filter(createBins())
-//        val actualMagnitudes = actual.map { it.magnitude }
-//
-//        for (bin in actual) {
-//            println(bin.magnitude)
-//        }
-//
-//        val expected = listOf(0F, 1F, 0F)
+    @Test
+    fun `magnitudes approach 0 as frequencies approach low pass frequency`() {
+        val sut = createSUT()
+
+
+        val actual = sut.filter(createBins(), lowCrossover, highCrossover)
+        val actualMagnitudes = actual.map { it.magnitude }
+
+        for (bin in actual) {
+            println(bin.magnitude)
+        }
+
+        val expected = listOf(0F, 1F, 0F)
 //        assertEquals(expected, actualMagnitudes)
-//    }
-//
-//    private fun createBins(): FrequencyBinList {
-//        val bins = mutableListOf<FrequencyBin>()
-//
-//        for (i in 1..160) {
-//            bins.add(FrequencyBin(i.toFloat(), 1F))
-//        }
-//
-//        return bins
-//    }
+    }
+
+    private fun createBins(): FrequencyBinList {
+        val bins = mutableListOf<FrequencyBin>()
+
+        for (i in 1..160) {
+            bins.add(FrequencyBin(i.toFloat(), 1F))
+        }
+
+        return bins
+    }
 }
