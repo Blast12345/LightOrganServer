2c1aaa8a84f45d7cf848e82661a5af6c59351e9d
Alex Larson <>
2023-11-25 15:33:07 -0600

    Proof of concept

--- src/main/kotlin/sound/frequencyBins/dominant/frequency/DominantFrequencyCalculator.kt
+++ src/main/kotlin/sound/frequencyBins/dominant/frequency/DominantFrequencyCalculator.kt
@@ -1,6 +1,12 @@
 package sound.frequencyBins.dominant.frequency
 
+import math.featureScaling.denormalize
+import math.featureScaling.normalizeLogarithmically
+import sound.frequencyBins.FrequencyBin
 import sound.frequencyBins.FrequencyBinList
+import sound.notes.Notes
+import kotlin.math.log
+import kotlin.math.pow
 
 class DominantFrequencyCalculator(
     private val peakFrequencyBinsFinder: PeakFrequencyBinsFinder = PeakFrequencyBinsFinder(),
@@ -10,9 +16,38 @@ class DominantFrequencyCalculator(
 
     fun calculate(frequencyBins: FrequencyBinList): Float? {
         val peakBins = getPeakFrequencyBins(frequencyBins)
-        val weightedMagnitude = weightedMagnitude(peakBins)
-        val totalMagnitude = totalMagnitude(peakBins)
-        return averageFrequency(weightedMagnitude, totalMagnitude)
+        val normalizedFrequencies = peakBins.map {
+            val normalizedFrequency = it.frequency.normalizeLogarithmically(
+                minimum = Notes.C.getFrequency(0),
+                maximum = Notes.C.getFrequency(1),
+                base = 2F
+            ) % 1
+
+            FrequencyBin(normalizedFrequency, it.magnitude)
+        }
+
+        val weightedMagnitude = weightedMagnitude(normalizedFrequencies)
+        val totalMagnitude = totalMagnitude(normalizedFrequencies)
+        val averageNormalizedFrequency = averageFrequency(weightedMagnitude, totalMagnitude) ?: return null
+
+        val freq = averageNormalizedFrequency.denormalizeLogarithmically(
+            minimum = Notes.C.getFrequency(0),
+            maximum = Notes.C.getFrequency(1),
+            base = 2F
+        )
+
+        println("Dominant Frequency $freq")
+
+        return freq
+    }
+
+    fun Float.denormalizeLogarithmically(minimum: Float, maximum: Float, base: Float): Float {
+        val power = this.denormalize(
+            minimum = log(minimum, base),
+            maximum = log(maximum, base)
+        )
+
+        return base.pow(power)
     }
 
     private fun getPeakFrequencyBins(frequencyBins: FrequencyBinList): FrequencyBinList {
