b02700a9f57fb9fe4c6216883c86528eff1d9a65
Alex Larson <>
2023-12-15 00:41:10 -0600

    Properly managed bass bins dependencies and added tests

--- src/main/kotlin/sound/frequencyBins/BassBinsFactory.kt
+++ src/main/kotlin/sound/frequencyBins/BassBinsFactory.kt
@@ -2,24 +2,29 @@ package sound.frequencyBins
 
 import config.ConfigSingleton
 import input.audioFrame.AudioFrame
+import sound.frequencyBins.filters.Crossover
 import sound.frequencyBins.filters.PassBandRegionFilter
 import sound.frequencyBins.listCalculator.FrequencyBinListCalculator
 
-// TODO:
 class BassBinsFactory(
-    private val passBandRegionFilter: PassBandRegionFilter = PassBandRegionFilter()
+    private val frequencyBinListCalculator: FrequencyBinListCalculator = FrequencyBinListCalculator(),
+    private val passBandRegionFilter: PassBandRegionFilter = PassBandRegionFilter(),
+    private val lowCrossover: Crossover = ConfigSingleton.lowCrossover,
+    private val highCrossover: Crossover = ConfigSingleton.highCrossover,
 ) {
 
     fun create(audioFrame: AudioFrame): FrequencyBinList {
-        val frequencyBins = FrequencyBinListCalculator().calculate(audioFrame.samples, audioFrame.format)
-        val lowCrossover = ConfigSingleton.lowCrossover
-        val highCrossover = ConfigSingleton.highCrossover
+        val frequencyBins = getFrequencyBins(audioFrame)
 
         return passBandRegionFilter.filter(
             frequencyBinList = frequencyBins,
             lowStopFrequency = lowCrossover.stopFrequency,
             highStopFrequency = highCrossover.stopFrequency
         )
+    }
+
+    private fun getFrequencyBins(audioFrame: AudioFrame): FrequencyBinList {
+        return frequencyBinListCalculator.calculate(audioFrame.samples, audioFrame.format)
     }
 
 }
