907e6c1a2f96e11285dc518b8e49e3e23d516634
Alex Larson <>
2023-12-28 20:46:47 -0600

    PeakOctaveBinsFinder tests

--- src/main/kotlin/sound/octave/PeakOctaveBinsFinder.kt
+++ src/main/kotlin/sound/octave/PeakOctaveBinsFinder.kt
@@ -1,39 +1,17 @@
 package sound.octave
 
-import math.featureScaling.normalizeLogarithmically
+import sound.bins.FrequencyToOctaveConverter
 import sound.frequencyBins.FrequencyBinList
 import sound.frequencyBins.dominant.frequency.PeakFrequencyBinsFinder
-import sound.notes.Notes
 
 class PeakOctaveBinsFinder(
-    private val peakFrequencyBinsFinder: PeakFrequencyBinsFinder = PeakFrequencyBinsFinder()
+    private val peakFrequencyBinsFinder: PeakFrequencyBinsFinder = PeakFrequencyBinsFinder(),
+    private val frequencyToOctaveConverter: FrequencyToOctaveConverter = FrequencyToOctaveConverter()
 ) {
 
-    private val rootNote = Notes.C
-
     fun find(frequencyBins: FrequencyBinList): OctaveBinList {
-        val peakBins = getPeakFrequencyBins(frequencyBins)
-
-        return peakBins.map {
-            OctaveBin(
-                position = it.frequency.normalizeToOctave() % 1,
-                magnitude = it.magnitude
-            )
-        }
-
-    }
-
-    private fun getPeakFrequencyBins(frequencyBins: FrequencyBinList): FrequencyBinList {
-        return peakFrequencyBinsFinder.find(frequencyBins)
-    }
-
-    // Reference: https://en.wikipedia.org/wiki/Octave
-    private fun Float.normalizeToOctave(): Float {
-        return this.normalizeLogarithmically(
-            minimum = rootNote.getFrequency(0),
-            maximum = rootNote.getFrequency(1),
-            base = 2F
-        )
+        val peakBins = peakFrequencyBinsFinder.find(frequencyBins)
+        return frequencyToOctaveConverter.convert(peakBins)
     }
 
 }
