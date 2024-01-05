f5bebddd48327a25bd62ee3dcaae16d098a0cbc0
Alex Larson <>
2024-01-04 20:37:27 -0600

    Calculating accurate fundamental note frequencies

--- src/main/kotlin/sound/notes/Note.kt
+++ src/main/kotlin/sound/notes/Note.kt
@@ -2,10 +2,12 @@ package sound.notes
 
 import kotlin.math.pow
 
-data class Note(val rootFrequency: Float) {
-
+data class Note(
+    val fundamentalFrequency: Float
+) {
+    
     fun getFrequency(octave: Int): Float {
-        return rootFrequency * 2F.pow(octave)
+        return fundamentalFrequency * 2F.pow(octave)
     }
 
 }
