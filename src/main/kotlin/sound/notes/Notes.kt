7cd547dea8736ccc872805b1fd1eae11faefa009
Blast12345 <germane.clipper.0q@icloud.com>
2024-01-04 20:52:23 -0600

    Calculate the fundamental frequency of notes instead of typing out imprecise values (#45)
    Co-authored-by: Alex Larson <>

--- src/main/kotlin/sound/notes/Notes.kt
+++ src/main/kotlin/sound/notes/Notes.kt
@@ -1,20 +1,33 @@
 package sound.notes
 
+import kotlin.math.pow
+
 object Notes {
 
-    // NOTE: This assumes A4=440hz tuning and C0
+    // I'm just assuming A4=440 Hz tuning for simplicity's sake.
     // Reference: https://pages.mtu.edu/~suits/notefreqs.html
-    val C = Note(rootFrequency = 16.35F)
-    val C_SHARP = Note(rootFrequency = 17.32F)
-    val D = Note(rootFrequency = 18.35F)
-    val D_SHARP = Note(rootFrequency = 19.45F)
-    val E = Note(rootFrequency = 20.60F)
-    val F = Note(rootFrequency = 21.83F)
-    val F_SHARP = Note(rootFrequency = 23.12F)
-    val G = Note(rootFrequency = 24.50F)
-    val G_SHARP = Note(rootFrequency = 25.96F)
-    val A = Note(rootFrequency = 27.50F)
-    val A_SHARP = Note(rootFrequency = 29.14F)
-    val B = Note(rootFrequency = 30.87F)
+    private const val notesInAnOctave = 12F
+    private const val tuningOctave = 4
+    private const val tuning = 440F
+    private val tuningFundamental = tuning * 2F.pow(-tuningOctave)
+
+    val C = createNote(-9)
+    val C_SHARP = createNote(-8)
+    val D = createNote(-7)
+    val D_SHARP = createNote(-6)
+    val E = createNote(-5)
+    val F = createNote(-4)
+    val F_SHARP = createNote(-3)
+    val G = createNote(-2)
+    val G_SHARP = createNote(-1)
+    val A = createNote(0)
+    val A_SHARP = createNote(1)
+    val B = createNote(2)
+
+    private fun createNote(positionRelativeToA: Int): Note {
+        return Note(
+            fundamentalFrequency = tuningFundamental * 2F.pow(positionRelativeToA / notesInAnOctave)
+        )
+    }
 
 }
