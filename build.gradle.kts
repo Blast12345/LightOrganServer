2ce4801f555d031ef305c7cde71fb65ff7f3f350
Blast12345 <germane.clipper.0q@icloud.com>
2025-10-08 14:16:56 -0500

    Fixed junit issues (I think?)

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,8 +21,9 @@ dependencies {
 
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
     testImplementation("io.mockk:mockk:1.13.4")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:6.0.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:6.0.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:6.0.0")
 }
 
 compose.desktop {
