b718359c9db13292631a5325180c530ad350c24d
Blast12345 <germane.clipper.0q@icloud.com>
2026-04-02 17:43:22 -0500

    Added equality to audio frame (#72)

--- src/main/kotlin/audio/samples/AudioFrame.kt
+++ src/main/kotlin/audio/samples/AudioFrame.kt
@@ -1,3 +1,22 @@
 package audio.samples
 
-class AudioFrame(val samples: FloatArray, val format: AudioFormat)
+// ENHANCEMENT: Introduce Samples type to make equality cleaner
+data class AudioFrame(
+    val samples: FloatArray,
+    val format: AudioFormat
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is AudioFrame) return false
+        return format == other.format && samples.contentEquals(other.samples)
+    }
+
+    // Generated automatically
+    override fun hashCode(): Int {
+        var result = samples.contentHashCode()
+        result = 31 * result + format.hashCode()
+        return result
+    }
+
+}
