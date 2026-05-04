9d9e70cbf89694e30f7c4cea81ef17ceb9cf63f4
Blast12345 <germane.clipper.0q@icloud.com>
2026-05-04 18:04:13 -0500

    Improved "nextPositiveNumber" behavior (#82)

--- src/test/kotlin/toolkit/monkeyTest/NextPositiveInt.kt
+++ src/test/kotlin/toolkit/monkeyTest/NextPositiveInt.kt
@@ -2,6 +2,8 @@ package toolkit.monkeyTest
 
 import kotlin.random.Random
 
-fun nextPositiveInt(min: Int = 1, max: Int = 1024): Int {
+fun nextInt(min: Int = 1, max: Int = 1024): Int {
+    require(min > 0) { "min must be a positive number" }
+
     return Random.nextInt(min, max)
 }
