38a2762682cbcd58ee52d993c90cdee40f23be21
Blast12345 <germane.clipper.0q@icloud.com>
2026-06-10 14:24:38 -0500

    JSON-RPC polishing

--- src/main/kotlin/jsonrpc/JsonRpcConnection.kt
+++ src/main/kotlin/jsonrpc/JsonRpcConnection.kt
@@ -3,19 +3,18 @@ package jsonrpc
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
 import tools.jackson.core.type.TypeReference
-import tools.jackson.databind.JsonNode
 import tools.jackson.module.kotlin.jacksonTypeRef
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 
-// REFERENCE: https://www.jsonrpc.org/specification
-// I could not cleanly represent IDs as both numbers and strings, so I picked strings.
-interface JsonRpcConnection {
+interface JsonRpcConnection : JsonRpcProtocol {
     val isConnected: StateFlow<Boolean>
     suspend fun connect()
     suspend fun disconnect()
+}
 
-    // Protocol
+// REFERENCE: https://www.jsonrpc.org/specification
+interface JsonRpcProtocol {
     val incomingNotifications: Flow<JsonRpcNotification>
     val incomingRequests: Flow<JsonRpcRequest>
 
@@ -23,18 +22,19 @@ interface JsonRpcConnection {
     suspend fun <T> sendRequest(method: String, params: Any?, responseType: TypeReference<T>, timeout: Duration = 5.seconds): T
     suspend fun respondWithSuccess(id: String, response: Any, timeout: Duration = 5.seconds)
     suspend fun respondWithFailure(id: String, code: Int, message: String, data: Any?, timeout: Duration = 5.seconds)
+
 }
+
+suspend inline fun <reified T> JsonRpcProtocol.sendRequest(
+    method: String,
+    params: Any? = null,
+    timeout: Duration = 5.seconds,
+): T = sendRequest(method, params, jacksonTypeRef<T>(), timeout)
+
 
 // Exceptions
 class RequestFailureException(
     val code: Int,
     override val message: String,
-    val data: JsonNode?
+    val data: Any?
 ) : Exception(message)
-
-// Convenience functions
-suspend inline fun <reified T> JsonRpcConnection.sendRequest(
-    method: String,
-    params: Any? = null,
-    timeout: Duration = 5.seconds,
-): T = sendRequest(method, params, jacksonTypeRef<T>(), timeout)
